c2d4a725b2b2b2ad35c82aae9621f33f74874cf0
Hígor Pinheiro Costa <higorcosta2001@outlook.com>
2023-02-07 12:34:46 -0300

    string

---
+++ modulo-02/4-string/string.js
@@ -0,0 +1,8 @@
+"use strict";
+// exemplo => Single Quotes
+let nomeC = 'Higor Costa';
+// exemplo => Double Quotes
+let cargo = "Higor Costa";
+// exemplo => Back Ticks
+let dados = `O funcionário ${nomeC}, esse funcionado tem o cargo de ${cargo}`;
+console.log(dados);
---
+++ modulo-02/4-string/string.ts
@@ -0,0 +1,9 @@
+// exemplo => Single Quotes
+let nomeC: string = 'Higor Costa';
+
+// exemplo => Double Quotes
+let cargo: string = "Higor Costa";
+
+// exemplo => Back Ticks
+let dados = `O funcionário ${nomeC}, esse funcionado tem o cargo de ${cargo}`;
+console.log(dados)
