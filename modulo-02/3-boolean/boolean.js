29a19c8deba58c033318dd5bc928e4b28ecddcf6
Hígor Pinheiro Costa <higorcosta2001@outlook.com>
2023-02-07 12:02:39 -0300

    Init type

---
+++ modulo-02/3-boolean/boolean.js
@@ -0,0 +1,17 @@
+"use strict";
+// exemplo 01
+//Boolean != boolean
+//Boolean (tipo Object)
+//boolean (tipo primitivo)
+let tarefaConcluida = true;
+let tarefaPendente = false;
+console.log(tarefaConcluida);
+console.log(tarefaPendente);
+// exemplo 02
+let concluido = false;
+if (concluido) {
+    console.log('Finalizado com sucesso');
+}
+else {
+    console.log('Pendente');
+}
---
+++ modulo-02/3-boolean/boolean.ts
@@ -0,0 +1,19 @@
+// exemplo 01
+//Boolean != boolean
+//Boolean (tipo Object)
+//boolean (tipo primitivo)
+
+let tarefaConcluida: boolean = true;
+
+let tarefaPendente: boolean = false;
+console.log(tarefaConcluida);
+console.log(tarefaPendente);
+
+// exemplo 02
+let concluido: boolean = false;
+
+if(concluido){
+    console.log('Finalizado com sucesso')
+}else{
+    console.log('Pendente')
+}
