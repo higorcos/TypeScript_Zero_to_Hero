d74dab2030b56b52c1078b9c50f0e558abffc1c2
Hígor Pinheiro Costa <higorcosta2001@outlook.com>
2023-02-26 21:39:55 -0300

    delete build na mesma pasta do .ts

--- modulo-02/4-string/string.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// exemplo => Single Quotes
-let nomeC = 'Higor Costa';
-// exemplo => Double Quotes
-let cargo = "Higor Costa";
-// exemplo => Back Ticks
-let dados = `O funcionário ${nomeC}, esse funcionado tem o cargo de ${cargo}`;
-console.log(dados);
