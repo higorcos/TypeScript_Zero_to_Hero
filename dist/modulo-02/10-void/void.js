a00872dced5ebf40cbe003d62ce4ffde80837f70
Hígor Pinheiro Costa <higorcosta2001@outlook.com>
2023-04-11 10:57:34 -0300

    dist/config

---
+++ dist/modulo-02/10-void/void.js
@@ -0,0 +1,10 @@
+"use strict";
+{
+}
+const logError = (errorMessage) => {
+    console.log(errorMessage);
+};
+logError('code: 505');
+let variavelExemploVoid;
+variavelExemploVoid = undefined;
+variavelExemploVoid = null;
---
+++ dist/modulo-02/11-null & Undefined/null&undefined.js
@@ -0,0 +1,11 @@
+"use strict";
+{
+}
+let vNull = null;
+console.log(vNull);
+console.log(typeof (vNull));
+let vUndefined = null;
+console.log(vUndefined);
+console.log(typeof (vUndefined));
+console.log('Exemplo 03.1', null == undefined);
+console.log('Exemplo 03.2', null === undefined);
