5c9446586081c38be9411f8b019a70ea93d10cd9
Hígor Pinheiro Costa <higorcosta2001@outlook.com>
2023-04-11 10:57:15 -0300

    Null

---
+++ modulo-02/11-null & Undefined/null&undefined.ts
@@ -0,0 +1,43 @@
+{/*
+
+            Null
+        -Ausência interncinal de valor
+        -valor indefinido
+        -Só aceita apenas um valor
+        -pode ser atribuido a uma variavel. porém, não aponta a nenhum objeto
+        -é um objeto
+        -é convertido em 0(zero) durante execuções de operações primitivas
+
+
+            Undefined
+        -Variáveis não inicializadas
+        -Ausência da propria variável
+        -Tem apenas um valor
+        -Função que não retorna um valor
+        -DEsclara a uma variavel. porém, não atribui nenhum valor.
+        -é um tipo (typeof undefined)
+        -è convertido em Nan (not a Number) durante execuções de operações primitivas
+
+*/}
+
+
+
+//==> Exemplo 01 - Null
+    let vNull = null;
+    console.log(vNull)
+    console.log(typeof(vNull))
+
+//==> Exemplo 02 - undefined
+
+    let vUndefined = null;
+    console.log(vUndefined)
+    console.log(typeof(vUndefined))
+  
+//==> Exemplo 03 - Diferenças e Similaridades
+
+console.log('Exemplo 03.1',null == undefined) //valor
+console.log('Exemplo 03.2',null === undefined) //valor e tipos
+
+   
+
+
